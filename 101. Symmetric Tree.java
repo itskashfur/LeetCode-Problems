91d72b1721fd9e88c996c396366e32f1350c965a
Kashfur Rahman <110445158+itskashfur@users.noreply.github.com>
2025-01-24 18:42:10 +0530

    101. Symmetric Tree.java

--- 101. Symmetric Tree.java
+++ 101. Symmetric Tree.java
@@ -1,25 +1,3 @@
-E
-tags: Tree, DFS, BFS
-time: O(n)
-space: O(n)
-
-检查tree是否symmetric
-
-注意Symmetric Binary Tree的例子和定义: 是镜面一样的对称. 并不是说左右两个sub-tree相等。
-
-#### Method1: DFS
-- Recursively check symmetrically相对应的Node.  
-- 每个node的children都和镜面另外一边相对的node的children刚好成镜面反射位置。
-
-#### Method2: interative with queue 
-- put left or right children in pair
-
-#### Method3: iterative with Stack
-- stack1: 左手边sub-tree先加left, 再加right child; 
-- stack2: 右手边sub-tree先加right child, 再加left child。   
-- process时，若symmetric，所有stack里面出来的node会一一对应。
-
-```
 /*
 Given a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
 
@@ -136,7 +114,6 @@ public class Solution {
       s1.push(node1.right);
       s2.push(node2.left);  
     }
-
     return true;
   }
 }
