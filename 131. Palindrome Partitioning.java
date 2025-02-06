6f6bcc0554c2cd6cf91340b7aed804afc60694a7
Kashfur Rahman <110445158+itskashfur@users.noreply.github.com>
2025-02-06 17:49:23 +0530

    131. Palindrome Partitioning.java

--- 131. Palindrome Partitioning.java
+++ 131. Palindrome Partitioning.java
@@ -1,34 +1,3 @@
-M
-tags: DFS, Backtracking
-time: O(2^n)
-space: O(n^2)
-
-给个string s, partition(分段)后, 要确保每个partition都是palindrome. 
-
-求所有partition palindrome组合. `list<list<string>>`
-
-#### DFS
-- 可以top->bottom: 遍历str, validate substring(start, i); if valid, add as candidate, and dfs; backtrack by remove candidate.
-- 也可以bottom->up: 遍历str, validate substring(start, i); if valid, dfs(remaining str), return list of suffix; cross match with curr candidate.
-
-#### DFS Top->Bottom
-- 在遍历str的时候，考虑从每个curr spot 到 str 结尾，是能有多少种palindorme?
-- 那就从curr spot当个字符开始算，开始backtracking.
-- 如果所选不是palindrome， 那move on.
-- 若所选的确是palindrome,　加到path里面，DFS去下个level，等遍历到了结尾，这就产生了一种分割成palindrome的串。
-- 每次DFS结尾，要把这一层加的所选palindrome删掉，backtracking嘛
-
-#### Optimization
-- 可以再每一个dfs level 算 isPalindrome(S), 但是可以先把 boolean[][] isPalin 算出来, 每次O(1) 来用
-- 注意: isPalin[i][j] 是 inclusive的, 所以用的时候要认准坐标
-- Calculate isPalin[i][j]: pick mid point [0 ~ n]
-- expand and validate palindrome at these indexes: `[mid, mid+1]` or `[mid-1][mid+1]`
-
-#### Complexity
-- Overall Space O(n^2): 存 isPlain[][]
-- Time O(2^n), 每一层都在做 pick/not pick index i 的选择, 所以worst case 2^n. 
-- 因为我们的isPalin[][]优化了palindrome的判断O(1), 所以overall Time: O(2^n)
-
 ```
 /*
 Given a string s, partition s such that every substring of the partition is a palindrome.
