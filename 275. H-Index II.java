49ef1c0c9e3bc19336074641f4ead63edfb93b95
Kashfur Rahman <110445158+itskashfur@users.noreply.github.com>
2025-04-23 19:55:27 +0530

    275. H-Index II.java

--- 275. H-Index II.java
+++ 275. H-Index II.java
@@ -1,45 +1,6 @@
-M
-tags: Binary Search
-time: O(logN)
-space: O(1) extra
-
-找到h-index, 给的citation int[] 已经sorted. h-index 的definition 具体看题目.
-
-Aim to find the lowest index mid, which maximize h = n - mid
-
-#### Binary Search
-- H-index的一个简单版, 已经sorted(从小到大), 找target value
-- 按定义, 找最后一个 `dictations[mid] >= h`, where `h = n - mid`
-- O(logn)
+275. H-Index II JAVA : Solution
 
 ```
-/*
-Follow up for H-Index: What if the citations array is sorted in ascending order? 
-Could you optimize your algorithm?
-Hint:
-
-Expected runtime complexity is in O(log n) and the input is sorted.
-
-Hide Company Tags Facebook
-Hide Tags Binary Search
-Hide Similar Problems (M) H-Index
-
-*/
-
-/*
-	citations[0,1,3,5,6]
-	look for a h, where x = N-h, arr[x] >= h
-	h is from right ot left.
-	We want to find smallest x that has arr[x] >= n-x
-	binary search:
-		start,mid,end
-		if match, keep going left until not able to
-	O(nLogN)
-*/
-
-
-// Binary Search, O(logn), 
-// Aim to find the lowest index mid, which maximize h = n - mid
 public class Solution {
     public int hIndex(int[] citations) {
         if (citations == null || citations.length == 0) {
