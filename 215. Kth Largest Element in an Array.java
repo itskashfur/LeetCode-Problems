eabc911c08ab65db4ee11ba603060adc9afc8e0d
Kashfur Rahman <110445158+itskashfur@users.noreply.github.com>
2025-05-09 22:54:08 +0530

    215. Kth Largest Element in an Array.java

--- 215. Kth Largest Element in an Array.java
+++ 215. Kth Largest Element in an Array.java
@@ -1,28 +1,4 @@
-M
-tags: Divide and Conquer, Heap, PriorityQueue, MinHeap, Quick Sort, Quick Select
-time: O(nlogk)
-space: O(k)
-
-kth largest in array
-
-#### PriorityQueue, MinHeap
-- Use minHeap to maintain PQ of k size and return PQ.peek()
-    - Maintain MinHeap: only allow larger elements (which will squzze out the min value)
-    - Remove peek() of queue if over size
-- O(nlogk)
-
-#### Quick Select, Quick Sort
-- 用Quick Sort 里面partion的一部分: sort结束后是ascending的.
-  - kth largest = (n - k)th smallest
-  - in partioned array (quick sort), the portion before pivot are less than pivot
-  - that is, the `pivot value` is the divider: anything after pivot is larger than it.
-  - after `swap(nums, low, pivot)`: index low has the (n-k)th smallest, if `low = n-k`
-- Steps:
-  - each iteration: pick pivot,然后从low,和high都和pivot作比较
-  - Find `low>pivot, high<pivot` to swap
-  - The new low is the next partion point
-- Time: average O(n), worst case O(n^2)
-- space: O(1) extra spaces besides recursive stack
+215. Kth Largest Element In An Array Problem : JAVA Solution
 
 ```
 /**
