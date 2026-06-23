8f36fdbe6b848c296780929aa7a9ec297b094fa2
Kashfur Rahman <110445158+itskashfur@users.noreply.github.com>
2026-06-23 19:59:43 +0530

    Time: 196 ms (97.50%) | Memory: 43 MB (100.00%) - LeetSync

--- 3962-number-of-zigzag-arrays-i/number-of-zigzag-arrays-i.java
+++ 3962-number-of-zigzag-arrays-i/number-of-zigzag-arrays-i.java
@@ -1,21 +1,26 @@
-class Solution {
+class Solution 
+{
     private static final int MOD = 1000000007;
-
-    public int zigZagArrays(int n, int l, int r) {
+    public int zigZagArrays(int n, int l, int r) 
+    {
+        //to hard this mf is
         int m = r - l + 1;
         int[] dp = new int[m];
         Arrays.fill(dp, 1);
 
-        for (int i = 2; i <= n; i++) {
+        for (int i = 2; i <= n; i++) 
+        {
             int sum = 0;
             if ((i & 1) == 0)
-                for (int j = 0; j < m; j++) {
+                for (int j = 0; j < m; j++) 
+                {
                     int t = dp[j];
                     dp[j] = sum;
                     sum = (sum + t) % MOD;
                 }
             else
-                for (int j = m - 1; j >= 0; j--) {
+                for (int j = m - 1; j >= 0; j--) 
+                {
                     int t = dp[j];
                     dp[j] = sum;
                     sum = (sum + t) % MOD;
