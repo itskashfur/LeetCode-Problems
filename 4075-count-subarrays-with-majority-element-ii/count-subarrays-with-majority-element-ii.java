ac69d572a344aad6346e07be320ed30307f6b26d
Kashfur Rahman <110445158+itskashfur@users.noreply.github.com>
2026-06-26 20:53:35 +0530

    Time: 2 ms (100.00%) | Memory: 88 MB (73.77%) - LeetSync

--- 4075-count-subarrays-with-majority-element-ii/count-subarrays-with-majority-element-ii.java
+++ 4075-count-subarrays-with-majority-element-ii/count-subarrays-with-majority-element-ii.java
@@ -2,13 +2,13 @@ class Solution
 {
     public long countMajoritySubarrays(int[] nums, int target) 
     {
+        //breeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeez
         int n = nums.length;
         int pref = n;
         int[] freq = new int[2 * n + 1];
         freq[n] = 1;
         long less = 0;
         long ans = 0;
-
         for (int num : nums) 
         {
             if (num == target) 
@@ -21,11 +21,9 @@ class Solution
                 pref--;
                 less -= freq[pref];
             }
-
             freq[pref]++;
             ans += less;
         }
-
         return ans;
     }
 }
