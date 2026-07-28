7ad9c525f2879a267ac31b344b42ac82561005a7
Kashfur Rahman <110445158+itskashfur@users.noreply.github.com>
2026-07-28 13:31:13 +0530

    Time: 29 ms (73.49%) | Memory: 48.4 MB (23.69%) - LeetSync

---
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+class Solution {
+    public String smallestPalindrome(String s) {
+        int n = s.length();
+        int halfLen = n / 2;
+        
+        // Step 1: Extract the first half of the string
+        char[] half = s.substring(0, halfLen).toCharArray();
+        
+        // Step 2: Sort the half lexicographically
+        Arrays.sort(half);
+        
+        String left = new String(half);
+        
+        // Step 3: Get middle character if length is odd
+        String mid = (n % 2 != 0) ? String.valueOf(s.charAt(halfLen)) : "";
+        
+        // Step 4: Reverse the sorted half to get the right mirror image
+        String right = new StringBuilder(left).reverse().toString();
+        
+        // Step 5: Combine into the final smallest palindromic string
+        return left + mid + right;
+    }
+}
