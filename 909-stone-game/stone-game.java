44f9404562cd35a17bff45511adb1c6922972eaf
Kashfur Rahman <110445158+itskashfur@users.noreply.github.com>
2026-08-02 21:40:20 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (25.83%) - LeetSync

---
+++ 909-stone-game/stone-game.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        // Alice can always force a win by choosing either 
+        // all even-indexed piles or all odd-indexed piles.
+        return true;
+    }
+}
