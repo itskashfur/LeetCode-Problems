10732e2a4a6596fe590ec1873a344583f289668d
Kashfur Rahman <110445158+itskashfur@users.noreply.github.com>
2026-08-11 00:20:59 +0530

    Time: 14 ms (91.38%) | Memory: 42.4 MB (78.33%) - LeetSync

--- 1617-stone-game-iv/stone-game-iv.java
+++ 1617-stone-game-iv/stone-game-iv.java
@@ -4,11 +4,9 @@ class Solution {
         
         for (int i = 1; i <= n; i++) {
             for (int k = 1; k * k <= i; k++) {
-                // If removing k*k stones leaves opponent in a losing state,
-                // the current player wins from state i.
                 if (!dp[i - k * k]) {
                     dp[i] = true;
-                    break; // No need to check other square numbers
+                    break;
                 }
             }
         }
