4b0aca6efa626bc4f29b8eb436272e8d299ee554
Kashfur Rahman <110445158+itskashfur@users.noreply.github.com>
2026-08-14 23:27:28 +0530

    Time: 2 ms (54.50%) | Memory: 43.5 MB (70.68%) - LeetSync

--- 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.java
+++ 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.java
@@ -8,7 +8,6 @@ class Solution {
             char ch = s.charAt(right);
             freq[ch - 'a']++;
 
-            // Shrink the window from the left if the current character occurs more than twice
             while (freq[ch - 'a'] > 2) {
                 freq[s.charAt(left) - 'a']--;
                 left++;
