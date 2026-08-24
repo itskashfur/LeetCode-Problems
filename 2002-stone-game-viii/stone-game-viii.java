7da20b640db544d261f92b2060c8e7ed8bf404f8
Kashfur Rahman <110445158+itskashfur@users.noreply.github.com>
2026-08-24 22:44:17 +0530

    Time: 3 ms (100.00%) | Memory: 86.8 MB (67.23%) - LeetSync

--- 2002-stone-game-viii/stone-game-viii.java
+++ 2002-stone-game-viii/stone-game-viii.java
@@ -2,20 +2,15 @@ class Solution {
     public int stoneGameVIII(int[] stones) {
         int n = stones.length;
         
-        // Compute prefix sums
         long[] pref = new long[n];
         pref[0] = stones[0];
         for (int i = 1; i < n; i++) {
             pref[i] = pref[i - 1] + stones[i];
         }
 
-        // Base case: picking all n stones
         long maxDiff = pref[n - 1];
 
-        // Process choices backwards from n - 2 down to 1
         for (int i = n - 2; i >= 1; i--) {
-            // maxDiff represents dp[i+1].
-            // We take the max between skipping index i (maxDiff) and taking index i (pref[i] - maxDiff).
             maxDiff = Math.max(maxDiff, pref[i] - maxDiff);
         }
 
